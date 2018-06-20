7e5293e44cdb644a2b913b134b763424b2d3679a
Valerio Bigiani <valerio.bigiani@gft.com>
2018-06-20 19:29:07 +0200

    Challenge 04

--- src/main/java/it/techgap/challenge/java/senior/Challenge04SSQL.java
+++ src/main/java/it/techgap/challenge/java/senior/Challenge04SSQL.java
@@ -1,8 +1,12 @@
 package it.techgap.challenge.java.senior;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * This test uses an in-memory <a href="http://www.h2database.com/">H2 database</a>
@@ -13,9 +17,50 @@ public class Challenge04SSQL {
      * TODO: Implement this
      */
     public static class ManagerWithCount {
+    	private String cf;
+    	
+    	private long count;
 
         public ManagerWithCount(String cf, long count) {
+        	this.cf = cf;
+        	this.count = count;
         }
+
+		public String getCf() {
+			return cf;
+		}
+
+		public long getCount() {
+			return count;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((cf == null) ? 0 : cf.hashCode());
+			result = prime * result + (int) (count ^ (count >>> 32));
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			ManagerWithCount other = (ManagerWithCount) obj;
+			if (cf == null) {
+				if (other.cf != null)
+					return false;
+			} else if (!cf.equals(other.cf))
+				return false;
+			if (count != other.count)
+				return false;
+			return true;
+		}
 
     }
 
@@ -28,7 +73,16 @@ public class Challenge04SSQL {
      * @throws SQLException If anything goes wrong
      */
     public static Collection<String> selectCFOfEmployeesAgedMoreThan(Connection connection, int minAge) throws SQLException {
-        return null;
+    	try (PreparedStatement st = connection.prepareStatement("SELECT CF FROM EMPLOYEE WHERE AGE > ?")) {
+    		st.setInt(1, minAge);
+    		try (ResultSet rs = st.executeQuery()) {
+    			List<String> result = new ArrayList<>();
+    			while (rs.next()) {
+    				result.add(rs.getString(1));
+    			}
+    			return result;
+    		}
+    	}
     }
 
     /**
@@ -43,7 +97,18 @@ public class Challenge04SSQL {
             Connection connection,
             int minSalary
     ) throws SQLException {
-        return null;
+    	try (PreparedStatement st = connection.prepareStatement("SELECT E.CF FROM EMPLOYEE E"
+    			+ " LEFT JOIN EMPLOYEE M ON E.MANAGER_CF = M.CF"
+    			+ " WHERE COALESCE(M.SALARY, 0) > ?")) {
+    		st.setInt(1, minSalary);
+    		try (ResultSet rs = st.executeQuery()) {
+    			List<String> result = new ArrayList<>();
+    			while (rs.next()) {
+    				result.add(rs.getString(1));
+    			}
+    			return result;
+    		}
+    	}
     }
 
     /**
@@ -59,7 +124,19 @@ public class Challenge04SSQL {
             Connection connection,
             int minSubordinates
     ) throws SQLException {
-        return null;
+    	try (PreparedStatement st = connection.prepareStatement("SELECT MANAGER_CF, COUNT(1) FROM EMPLOYEE "
+    			+ " WHERE MANAGER_CF IS NOT NULL"
+    			+ " GROUP BY MANAGER_CF"
+    			+ " HAVING COUNT(1) > ?")) {
+    		st.setInt(1, minSubordinates);
+    		try (ResultSet rs = st.executeQuery()) {
+    			List<ManagerWithCount> result = new ArrayList<>();
+    			while (rs.next()) {
+    				result.add(new ManagerWithCount(rs.getString(1), rs.getLong(2)));
+    			}
+    			return result;
+    		}
+    	}
     }
 
 }
