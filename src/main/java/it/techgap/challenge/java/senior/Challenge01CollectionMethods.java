27b794f11ebb96461559f176c4fb47b4d236bc59
Valerio Bigiani <valerio.bigiani@gft.com>
2018-06-20 18:47:24 +0200

    Challenge 01

--- src/main/java/it/techgap/challenge/java/senior/Challenge01CollectionMethods.java
+++ src/main/java/it/techgap/challenge/java/senior/Challenge01CollectionMethods.java
@@ -1,38 +1,70 @@
 package it.techgap.challenge.java.senior;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Challenge01CollectionMethods {
 	
 	public static int max(int[] in){
-		return -1;
+		if (in == null) {
+			return Integer.MIN_VALUE;
+		}
+		return Arrays.stream(in).max().orElse(Integer.MIN_VALUE);
 	}
 	
 	public static int min(int[] in){
-		return -1;
+		if (in == null) {
+			return Integer.MAX_VALUE;
+		}
+		return Arrays.stream(in).min().orElse(Integer.MAX_VALUE);
 	}
 	
 	public static int[] sortIt(int[] in){
-		return null;
+		if (in == null) {
+			return null;
+		}
+		int[] result = Arrays.copyOf(in, in.length);
+		Arrays.sort(result);
+		return result;
 	}
 	
 	public static Boolean[] sortIt(Boolean[] in){
-		return null;
+		if (in == null) {
+			return null;
+		}
+		Boolean[] result = Arrays.copyOf(in, in.length);
+		Arrays.sort(result, Comparator.reverseOrder());
+		return result;
 	}
 	
 	public static int[] removeNegative(int[] in){
-		return null;
+		if (in == null) {
+			return null;
+		}
+		return sortIt(Arrays.stream(in).filter(i -> i >= 0).toArray());
 	}
 	
 	public static int count(String[] in, String e){
-		return -1;
+		if (in == null) {
+			return 0;
+		}
+		return (int) Arrays.stream(in).filter(s -> Objects.equals(s, e)).count();
 	}
 	
 	public static int maxRepetitions(String[] in){
-		return -1;
+		if (in == null) {
+			return 0;
+		}
+		Map<Object, List<String>> values = Arrays.stream(in).collect(Collectors.groupingBy(x -> x));
+		return values.values().stream().map(List::size).max(Comparator.naturalOrder()).orElse(0);
 	}
 	
 	public static int[] mergeAndSort(int[]... in){
-		return null;
+		return sortIt(Arrays.stream(in).flatMapToInt(Arrays::stream).distinct().toArray());
 	}
 
 }
