198304db8930ec21a2aaf5d9351e7b61ca99aac3
Valerio Bigiani <valerio.bigiani@gft.com>
2018-06-20 20:06:50 +0200

    Challenge 06

--- src/main/java/it/techgap/challenge/java/senior/Challenge06Extra.java
+++ src/main/java/it/techgap/challenge/java/senior/Challenge06Extra.java
@@ -2,6 +2,11 @@ package it.techgap.challenge.java.senior;
 
 public class Challenge06Extra {
     public static String splitAndReverse(String i, int positions) {
-        return null;
+    	String left = i.substring(0, positions);
+    	String right = i.substring(positions);
+    	
+    	StringBuilder sb = new StringBuilder(right).reverse();
+    	sb.append(left);
+    	return sb.toString();
     }
 }
